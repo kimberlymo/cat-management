85bee71b79d0fec935ada363fe06adcd09074fed
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-06-27 16:33:54 +0200

    first commit

---
+++ src/main/java/ch/bbw/modul133/catmanagement/CatManagementApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.modul133.catmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/modul133/catmanagement/CatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.modul133.catmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
